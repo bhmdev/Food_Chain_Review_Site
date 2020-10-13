8d74fde5b0a8d141612d7ef871961c7d6b3e08b0
bhmdev <bmontima@gmail.com>
2020-10-13 18:29:40 -0400

    Changes that were requested

--- src/main/resources/db/migration/V1__create_initial_tables.sql
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE food_chains (
     id SERIAL PRIMARY KEY,
-     name VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
     description VARCHAR(255),
     img_url TEXT NOT NULL
 );
 
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
-    comment VARCHAR NOT NULL,
-    rating  INTEGER CHECK (rating <= '1' >= '5') NOT NULL,
-    reviews INTEGER REFERENCES food_chains(id)
+    comment VARCHAR,
+    rating  INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
+    food_chain_id INTEGER REFERENCES food_chains(id) NOT NULL
 );
