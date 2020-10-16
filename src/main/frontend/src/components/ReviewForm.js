75cab7b2f2459d7669ee878dcd5b98f461d440f0
bhmdev <bmontima@gmail.com>
2020-10-16 11:49:56 -0400

    Here is the minimal minimal review form

---
+++ src/main/frontend/src/components/ReviewForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+
+const ReviewForm = props => {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+    
+    
+    return (
+  <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" name="Comment" placeholder="Comment" ref={register} />
+        <input type="range" placeholder="Rating " name="Rating" ref={register({max: 5, min: 0})} />
+  
+        <input type="submit" />
+      </form>
+  </div>
+    );
+  }
+
+  export default ReviewForm
