aa14c7d68e28c1900c379d77d7b1b342797c0590
kducasse <karaducasse@gmail.com>
2020-10-19 22:49:27 -0400

    Review and Chain ables are rendered from the table container component

---
+++ src/main/frontend/src/components/TableContainer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import FoodChainTable from "./FoodChainTable";
+import ReviewTable from "./ReviewTable";
+import TabPanel from "./TabPanel";
+
+const TableContainer = () => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} id="table-container">
+          <Tab label="Food Chains Table" />
+          <Tab label="Reviews Table" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <FoodChainTable/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <ReviewTable/>
+      </TabPanel>
+    </>
+  );
+}
+
+export default TableContainer
--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
       {
         test: /\.scss$/,
         use: [
